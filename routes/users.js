e2581da189cd974bf97ca538ddd147600e65e9f9
randrerd <raulromand@gmail.com>
2020-08-26 22:05:45 -0300

    :recycle: refactor: Modify users route file for controller use

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/database');
+const UsersController = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth')
 
 const User = require('../models/User');
 
@@ -10,28 +12,7 @@ router.get('/', (req, res)=> {
     })
 })
 
-router.post('/register', (req,res )=> {
-    const data = req.body;
-
-    let {username, firstname, lastname, email, phone,address, password, is_admin} = data;
-
-    User.create({
-        username,
-        firstname,
-        lastname,
-        email,
-        phone,
-        address,
-        password,
-        is_admin
-    })
-    .then((user) => res.status(200).json({
-        message: 'User created',
-        user: user
-    }))
-    .catch((err)=> {
-        console.log(err)
-    })
-})
-
+router.post('/register', UsersController.users_register)
+router.post('/login', UsersController.users_login)
+router.delete('/delete/:userId', checkAuth, UsersController.users_delete);
 module.exports = router;
