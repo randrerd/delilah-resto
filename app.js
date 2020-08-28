96bc635b2c1e29fd67c40253b4813fb702c5895c
randrerd <raulromand@gmail.com>
2020-08-28 11:13:47 -0300

    :speech_balloon: chore: Prettify code

--- app.js
+++ app.js
@@ -7,24 +7,25 @@ const cookieParser = require('cookie-parser');
 const dotenvResult = dotenv.config();
 const app = express();
 
-
 //Databse
 const db = require('./db/database');
 
 //Try db connection
-db.authenticate().then(() => console.log('db connected')).catch(err => console.log(err))
+db.authenticate()
+  .then(() => console.log('db connected'))
+  .catch((err) => console.log(err));
 
 //Basic middleware
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-
-app.use('/users', require('./routes/users'))
+app.use('/users', require('./routes/users'));
+app.use('/products', require('./routes/products'));
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
-    console.log('Server start')
-})
+app.listen(PORT, () => {
+  console.log('Server start');
+});
