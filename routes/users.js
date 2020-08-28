c37a0c7f4c50c1dc1c5f877971150201ed59167a
randrerd <raulromand@gmail.com>
2020-08-28 09:10:54 -0300

    :sparkles: feat: Add get user details endpoint and checkAdmin middleware

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db/database');
 const UsersController = require('../controllers/users');
 const checkAuth = require('../middleware/checkAuth')
+const checkAdmin = require('../middleware/checkAdmin')
 
 const User = require('../models/User');
 
@@ -11,8 +12,9 @@ router.get('/', (req, res)=> {
         message: 'You here'
     })
 })
-
+router.get('/:userId', checkAuth, UsersController.users_get_user);
 router.post('/register', UsersController.users_register)
 router.post('/login', UsersController.users_login)
-router.delete('/delete/:userId', checkAuth, UsersController.users_delete);
+router.delete('/delete/:userId', checkAdmin, UsersController.users_delete);
+
 module.exports = router;
