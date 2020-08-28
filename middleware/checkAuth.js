e2ba965ea33763a455aa1b040b1ace1c93c3a98d
randrerd <raulromand@gmail.com>
2020-08-28 09:09:41 -0300

    :recycle: refactor: Change authorization method from using headers to cookies

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,12 +1,26 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
-    req.userData = decoded;
+    const { cookies } = req;
+    const userIdToAccess = req.params.userId;
 
-    next();
+    if ('session_id' in cookies) {
+      const token = cookies.session_id;
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+      let isPermitted = userIdToAccess == decoded.userId;
+      let isAdmin = await User.findOne({
+        where: { id: decoded.userId, is_admin: 1 },
+      });
+
+      if (!isPermitted) {
+        if (!isAdmin) {
+          return res.status(403).json({ message: 'Access Forbidden' });
+        } else next();
+      } else next();
+    }
   } catch (error) {
     return res.status(401).json({
       message: 'Auth failed',
