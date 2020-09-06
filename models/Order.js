5d0d39e572faa5ca6594c5d606ca069b70ee2e56
randrerd <raulromand@gmail.com>
2020-09-06 19:25:55 -0300

    :goal_net: chore(models): Modify Order model to match database changes

--- models/Order.js
+++ models/Order.js
@@ -2,14 +2,21 @@ const { Sequelize } = require('sequelize');
 const db = require('../db/database');
 
 const Order = db.define('order', {
-  status_id: {
-    type: Sequelize.INTEGER,
+  status: {
+    type: Sequelize.STRING,
+    defaultValue: 'Nuevo',
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  total: {
+    type: Sequelize.DECIMAL,
   },
   user_id: {
     type: Sequelize.INTEGER,
   },
-  payment_method_id: {
-    type: Sequelize.INTEGER,
+  payment_method: {
+    type: Sequelize.STRING,
   },
 });
 
