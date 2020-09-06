74f0b0fead042fd2a9282b1a59142907e9fe50cd
randrerd <raulromand@gmail.com>
2020-09-06 19:21:46 -0300

    :goal_net: chore: Add restrictions to User model

--- models/User.js
+++ models/User.js
@@ -4,27 +4,36 @@ const db = require('../db/database');
 const User = db.define('user', {
   username: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   firstname: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   lastname: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   email: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   phone: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   address: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   password: {
     type: Sequelize.STRING,
+    allowNull: false,
   },
   is_admin: {
     type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: 0,
   },
 });
 
